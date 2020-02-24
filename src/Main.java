d6e9044e2e8dcab295b0abe5213756babcf0c644
C02793 <C02793@g-able.com>
2020-02-24 18:24:38 +0700

    Add method leap year

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int[] month1 = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int[] month2 = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] monthNotLeapYear = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //365
+        int[] monthLeapYear = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};  //266
 
         String input = "14/02/2563";
 
         String[] valentine = input.split("/");
         int thisAD = Integer.parseInt(valentine[2])-543;
 
-
+        if (isLeapYear(thisAD)){
+            IntStream.of(monthLeapYear).sum();
+        }
     }
 
-    public boolean isLeapYear(int year){
+    public static boolean isLeapYear(int year){
         return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 }
